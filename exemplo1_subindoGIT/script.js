62298e979abd75e53d5716533ddb4bd8d3b5f1e1
Cauê <caue.cola@aluno.senai.br>
2025-06-18 12:17:47 -0300

    Incluindo nota 3

--- exemplo1_subindoGIT/script.js
+++ exemplo1_subindoGIT/script.js
@@ -4,15 +4,16 @@ function CalcularMedia() {
     // Pega os valores dos campos
     var n1 = parseFloat(document.getElementById('Nota1').value);
     var n2 = parseFloat(document.getElementById('Nota2').value);
- 
-        // Verifica se são números válidos
-    if (isNaN(n1) || isNaN(n2)) {
+    var n3 = parseFloat(document.getElementById('Nota3').value);
+        
+    // Verifica se são números válidos
+    if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
         alert("Por favor, insira notas válidas!");
         return;
     }
  
     // Calcular a média
-    var media = (n1 + n2) / 2;
+    var media = (n1 + n2 + n3) / 3;
  
     // Mostrar o resultado
     document.getElementById('resultado').innerText = "A média é: " + media.toFixed(2);
